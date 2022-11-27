27a4e050a435f43e40b8d62ba1fb1fb1673658cf
Abhishek Singhal <abhisinghal112001@gmail.com>
2022-11-27 20:33:24 +0530

    linear

--- src/Search/Linear.java
+++ src/Search/Linear.java
@@ -10,10 +10,17 @@ public class Linear {
         System.out.print("Enter the length of the array:- ");
         int n = s.nextInt();
         int[] arr = new int[n];
-        System.out.println("'Enter sorted array for binary search'");
         AO.ins(arr,n);
         System.out.print("Enter target element: - ");
         int target = s.nextInt();
-//        System.out.println("Target found at: - "+(linear(arr,0,n-1,target)+1));
+        System.out.println("Target found at: - "+(linear(arr,target)+1));
+    }
+    static int linear(int arr[],int target){
+        for (int i=0;i<arr.length;i++) {
+            if(arr[i]==target) {
+                return i;
+            }
+        }
+        return -2;
     }
 }
