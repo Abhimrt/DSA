3366463bf586120c2bc6847de603f3a3b65e002d
Abhishek Singhal <abhisinghal112001@gmail.com>
2022-11-29 21:11:02 +0530

    josephus

---
+++ src/Recursion/JosephusProblem.java
@@ -0,0 +1,10 @@
+package Recursion;
+
+public class JosephusProblem {
+    public static void main(String[] args) {
+        System.out.println(jos(5,3));
+    }
+    static int jos(int n,int k){
+        return n==1?0:(jos(n-1,k)+k)%n;
+    }
+}
