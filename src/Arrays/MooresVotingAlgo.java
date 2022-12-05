0b1404bb6109567e79f7953527f8b3b2a637e718
Abhishek Singhal <abhisinghal112001@gmail.com>
2022-12-05 19:51:29 +0530

    moorees voting algo

--- src/Arrays/MooresVotingAlgo.java
+++ src/Arrays/MooresVotingAlgo.java
@@ -3,6 +3,30 @@ package Arrays;
 // we find the element which is coming more than n/2 times
 public class MooresVotingAlgo {
     public static void main(String[] args) {
-        System.out.println();
+        int arr[] = new int[] {1,2,3,4,1};
+        System.out.println(majority(arr));
+    }
+    static int majority(int[] arr){
+        int sum = 0;
+        int target = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]==arr[target]){
+                sum++;
+            }else{
+                if(sum==1){
+                    target = i;
+                }else{
+                    sum--;
+                }
+            }
+        }
+        sum = 0;
+        for (int i=0;i<arr.length;i++) {
+            if(arr[i]==arr[target]){
+                sum++;
+            }
+        }
+        return (arr.length/2<sum)?arr[target]:-1;
+
     }
 }
