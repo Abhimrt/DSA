99631740ee3d2b6ff0f9ef9d90f847ee20f53723
Abhimrt <abhisinghal112001@gmail.com>
2022-12-30 21:28:42 +0530

    Bubble sort

---
+++ src/Sorting/BubbleSort.java
@@ -0,0 +1,22 @@
+package Sorting;
+
+import Basic.AO;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = AO.ins();
+        for(int i=0;i<arr.length;i++){
+            boolean isSwap = false;
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    AO.swap(arr,j,j+1);
+                    isSwap=true;
+                }
+            }
+            if(!isSwap){
+                break;
+            }
+        }
+        AO.show(arr);
+    }
+}
